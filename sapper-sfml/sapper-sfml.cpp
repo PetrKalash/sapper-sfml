81a777bdedaa119c736614c6e52daa91e4d69281
PetrKalash <daniil.neshkov2003@mail.ru>
2023-01-12 12:11:34 +0300

    Добавьте файлы проекта.

---
+++ sapper-sfml/sapper-sfml.cpp
@@ -0,0 +1,12 @@
+﻿// sapper-sfml.cpp: определяет точку входа для приложения.
+//
+
+#include "sapper-sfml.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
